bf09c4d3e1499c872e614fdd8ec2904eeef80018
Christian Adell <chadell@gmail.com>
2023-01-25 15:03:36 +0100

    build: :construction_worker: Implement custom go-nautobot versioning process (#12)
    * build: :construction_worker: Implement custom go-nautobot versioning process
    
    It addresses https://github.com/nautobot/go-nautobot/issues/7
    
    * fix script
    
    * do no generate binding for experimental Nautobot versions (for now)

--- development/generate.sh
+++ development/generate.sh
@@ -3,6 +3,20 @@ set -euxo pipefail
 
 echo "Starting generation of Go Nautobot Bindings"
 
+BETA_TAG="beta"
+ALPHA_TAG="alpha"
+RC_TAG="rc"
+
+# TODO: eventually we would like to generate for experimental Nautobot versions
+if [[ "$NAUTOBOT_VER" == *"$BETA_TAG"* ]] || [[ "$NAUTOBOT_VER" == *"$ALPHA_TAG"* ]] || [[" $NAUTOBOT_VER" == *"$RC_TAG"* ]]; then
+  echo "${NAUTOBOT_VER} is not an official Nautobot version, no new bindings are generated."
+  exit 0
+fi
+
+VERSION_FILE="/client/tag.md"
+CURRENT_VERSION=$(head -n 1 $VERSION_FILE)
+CURRENT_MAJOR_MINOR_VER=${CURRENT_VERSION%.*}
+
 # Using only major and minor version to get the api_version
 # 1.3.3 -> 1.3
 # 1.3.7 -> 1.3
@@ -13,7 +27,22 @@ wget --tries=5  http://nautobot:8080/api/swagger.yaml?api_version=${MAJOR_MINOR_
 
 oapi-codegen --config oapi-config.yml swagger.yaml
 
-echo $NAUTOBOT_VER > tag.md
+if [ "$CURRENT_MAJOR_MINOR_VER" = "$MAJOR_MINOR_VER" ]; then
+    # Get the Patch version string
+    NEW_PATCH_VERSION=$(echo $CURRENT_VERSION | awk -F '.' '{ print $3;}')
+    # Remove suffixes in version
+    NEW_PATCH_VERSION=${NEW_PATCH_VERSION%-*}
+    # Increment Patch version string with 1
+    NEW_PATCH_VERSION=$((${NEW_PATCH_VERSION} + 1))
+    NEW_TAG=${CURRENT_MAJOR_MINOR_VER}.$NEW_PATCH_VERSION
+else
+    NEW_TAG=${MAJOR_MINOR_VER}.0
+fi
+
+# TODO: remove beta when it's in production
+FINAL_NEW_TAG=${NEW_TAG}-beta
+
+echo $FINAL_NEW_TAG > tag.md
 
 cp tag.md /client
 cp *.go /client
