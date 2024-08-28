82790ca96d5f5026fc8eb25cfaa4fad633030789
TobiPeterG <github.threefold020@passmail.net>
2024-08-28 14:57:38 +0200

    remove unused patches

--- development/scripts/fix-spec.py
+++ development/scripts/fix-spec.py
@@ -73,23 +73,6 @@ if 'components' in data and 'schemas' in data['components']:
                     }
                     
         if 'properties' in schema:
-            # Remove "null" item from nullable enums
-            for name, prop in schema['properties'].items():
-                if 'enum' in prop and None in prop['enum']:
-                    prop['enum'].remove(None)
-                if 'properties' in prop and 'value' in prop['properties'] and 'enum' in prop['properties']['value'] and None in prop['properties']['value']['enum']:
-                    prop['properties']['value']['enum'].remove(None)
-
-            # Fix nullable types
-            nullable_types = [
-                'parent_device',
-                'primary_ip',
-            ]
-
-            for ntype in nullable_types:
-                if ntype in schema['properties']:
-                    schema['properties'][ntype]['nullable'] = True
-
             # Fix non-nullable types
             # See: https://github.com/OpenAPITools/openapi-generator/issues/18006
             non_nullable_types = [
